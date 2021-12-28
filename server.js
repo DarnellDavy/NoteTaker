dc2cbb64770d6070563616228ced328ad6eaa980
Darnell Davy <darnelldavy@darnells-mbp.myfiosgateway.com>
2021-12-28 17:12:15 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
+
+const PORT =    process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => 
+    console.log(`App now listening at http://localhost: ${PORT} 🚀`)
+);
